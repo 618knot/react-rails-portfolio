ee28e4f23944d53d504096ffcba7654f5b92014e
618knot <knot618@gmail.com>
2023-05-03 19:42:23 +0900

    api叩く処理追加

--- web/app/controllers/home_controller.rb
+++ web/app/controllers/home_controller.rb
@@ -1,4 +1,32 @@
 class HomeController < ApplicationController
+  ZENN_USER_NAME = "knot"
+  CONNPASS_USER_NAME = "knot"
+  GITHUB_USER_NAME = "618knot"
+
   def index
+  end
+
+  def call_api(url)
+    http_client = HTTPClient.new
+    response = http_client.get(url)
+    return response.body
+  end
+
+  def zenn_api
+    url = "https://zenn.dev/api/articles?username=#{ZENN_USER_NAME}&order=latest"
+    response = call_api(url)
+    render json: JSON.parse(response)
+  end
+
+  def connpass_api
+    url = "https://connpass.com/api/v1/event/?nickname=#{CONNPASS_USER_NAME}"
+    response = call_api(url)
+    render json: JSON.parse(response)
+  end
+
+  def  github_api
+    url = "https://api.github.com/users/#{GITHUB_USER_NAME}/repos"
+    response = call_api(url)
+    render json: JSON.parse(response)
   end
 end
--- web/config/routes.rb
+++ web/config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   root 'home#index'
+  get '/zenn', to: 'home#zenn_api'
+  get '/connpass', to: 'home#connpass_api'
+  get '/github', to: 'home#github_api'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
